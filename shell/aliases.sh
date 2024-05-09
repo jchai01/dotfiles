09b3fe7bcf65dc0ac3c2ecc7480df191f7d46ca9
Joseph <josephchai100@hotmail.com>
2024-05-09 18:43:54 +0100

    .

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,15 +1,15 @@
 alias r='source ~/.bashrc' # reload bash
-alias a="nvim /home/jchai/repos/dotfiles/shell/git_aliases.sh"
-alias v="/home/jchai/Downloads/nvim-linux64/bin/nvim"
+alias a="nvim $HOME/repos/dotfiles/shell/aliases.sh"
+alias v="nvim"
 alias u="sudo apt update && sudo apt upgrade"
 alias o="xdg-open"
 alias ..="cd .."
 alias t="tmuxifier"
 
 # go to path
-alias dot="cd /home/jchai/repos/dotfiles/shell"
-alias vp="cd /home/jchai/.config/nvim" # vim path
-alias tmp="cd /home/jchai/.tmuxifier/layouts" # tmuxifier path
+alias dot="cd $HOME/repos/dotfiles/shell"
+alias vp="cd $HOME/.config/nvim" # vim path
+alias tmp="cd $HOME/.tmuxifier/layouts" # tmuxifier path
 
 # Git alias
 alias gs='git status'
