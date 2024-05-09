fac7f6dbccc5d59e33f37ab72014f6a8cce96eb3
Joseph <josephchai100@hotmail.com>
2024-05-09 18:10:33 +0100

    .

--- shell/git_aliases.sh
+++ shell/git_aliases.sh
@@ -1,5 +1,15 @@
-alias open="xdg-open"
-alias update="sudo apt update && sudo apt upgrade"
+alias r='source ~/.bashrc' # reload bash
+alias a="nvim /home/jchai/repos/dotfiles/shell/git_aliases.sh"
+alias v="/home/jchai/Downloads/nvim-linux64/bin/nvim"
+alias u="sudo apt update && sudo apt upgrade"
+alias o="xdg-open"
+alias ..="cd .."
+alias t="tmuxifier"
+
+# go to path
+alias dot="cd /home/jchai/repos/dotfiles/shell"
+alias vp="cd /home/jchai/.config/nvim" # vim path
+alias tmp="cd /home/jchai/.tmuxifier/layouts" # tmuxifier path
 
 # Git alias
 alias gs='git status'
@@ -8,7 +18,5 @@ alias gl='git log'
 alias gc='git checkout'
 alias gp='git push'
 alias gpl='git pull'
-alias dotcommit="git add . && git commit -m '.'"
-
-# open lazygit
+alias dcom="git add . && git commit -m '.'" # dotcommit
 alias lg="lazygit"
