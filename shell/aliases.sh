0b295e468e7ae63c8fb46e1a51ac00f982b5f785
Joseph <josephchai100@hotmail.com>
2024-05-20 01:46:37 +0100

    .

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,24 +1,37 @@
 alias r='source ~/.bashrc' # reload bash
-alias a="nvim $HOME/repos/dotfiles/shell/aliases.sh"
+alias a="nvim ~/repos/dotfiles/shell/aliases.sh"
 alias v="nvim"
 alias u="sudo apt update && sudo apt upgrade"
 alias o="xdg-open"
-alias ..="cd .."
 alias t="tmuxifier"
+alias b="nvim ~/.bashrc"
+alias hist="history"
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
+alias grep="grep -i" # grep always searching in case insensitive mode
+alias sys='sudo systemctl'
 
-# go to path
-alias dot="cd $HOME/repos/dotfiles/shell"
-alias vp="cd $HOME/.config/nvim" # vim path
-alias tp="cd $HOME/.tmuxifier/layouts" # tmuxifier path
+# navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias dot="cd ~/repos/dotfiles/shell"
+alias vp="cd ~/.config/nvim" # vim path
+alias tp="cd ~/.tmuxifier/layouts" # tmuxifier path
 
-# Git alias
+# git alias
 alias gs='git status'
 alias gd='git diff'
-alias gl='git log'
+alias gl='git log --pretty=oneline -n 20 --graph --abbrev-commit'
 alias gc='git checkout'
+alias gcl='git clone'
 alias gp='git push'
 alias gpl='git pull'
 alias dcom="git add . && git commit -m '.'" # dotcommit
 alias lg="lazygit"
+
+# temp
+alias api="cd ~/repos/LandFutures-API/"
